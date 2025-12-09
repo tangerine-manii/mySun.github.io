d19f91346025bd2eab50ade2b4cb25f77c19e344
tangerine-manii <rkdrb98@gmail.com>
2025-12-09 19:27:54 +0900

    Update save-form.js

--- netlify/functions/save-form.js
+++ netlify/functions/save-form.js
@@ -1,12 +1,12 @@
-import { neon } from '@netlify/neon';
+import { neon } from '@neondatabase/serverless';
 
-export default async (event, context) => {
-  const sql = neon(); // automatic NETLIFY_DATABASE_URL
+export default async () => {
+  const sql = neon(process.env.NETLIFY_DATABASE_URL);
 
-  const [result] = await sql`SELECT now()`;
+  const rows = await sql`SELECT * FROM posts`;
 
   return {
     statusCode: 200,
-    body: JSON.stringify(result)
+    body: JSON.stringify(rows)
   };
 };
